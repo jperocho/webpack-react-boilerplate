602fe5c4142873e229c735a8ea52ce2310cc0e76
John Mark Perocho <per.eight@gmail.com>
2017-04-30 23:56:27 +0800

    Added React Babel and changed name to Webpack React Boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,24 @@ const path = require('path');
 const desc = require('./package').description;
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js'
+    },
     output: {
-        path: '/Users/Bhong/Sandbox/webpack-practice/dist',
+        path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js'
+    },
+    devServer: {
+        stats: 'errors-only',
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: 'babel-loader' 
+            }
+        ]
     },
     plugins: [new HtmlWebpackPlugin({
         title: desc,
