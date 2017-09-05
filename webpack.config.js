3e66c6eac156fb4b4ed96a84fd005c442540c864
John Mark A. Perocho <per.eight@gmail.com>
2017-09-05 17:01:03 +0800

    Updated Packages, Fix route issue cannot Get on refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,3 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const desc = require('./package').description;
-
-
-// module.exports = {
-//     entry: {
-//         app: './src/index.js'
-//     },
-//     output: {
-//         path: path.resolve(__dirname,'dist'),
-//         publicPath: '/', // Fixed the missing bundle.js on React Router
-//         filename: 'bundle.js'
-//     },
-//     devServer: {
-//         stats: 'errors-only',
-//         open: true,
-//         historyApiFallback: true // Fixed Error "Cannot GET" on React Router
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.js$/,
-//                 exclude: /node_modules/,
-//                 use: "babel-loader"
-//             },
-//             {
-//                 test: /\.s?css$/,
-//                 use: ["style-loader","css-loader","sass-loader"]
-//             }
-//         ]
-//     },
-//     plugins: [new HtmlWebpackPlugin({
-//         title: desc,
-//         hash: true,
-//         template: './src/index.html'
-//     })]
-// }
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const desc = require('./package').description;
@@ -45,6 +7,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    devServer: {
+      historyApiFallback: true
     },
     module: {
       rules: [
